492e6ad2dd7e138fd52bff793074c56fa473c295
Yusuf Amer <impara1@gmail.com>
2025-03-13 12:54:00 +0100

    Renaming the project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="luma_video_pipeline",
+    name="video_pipeline",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
